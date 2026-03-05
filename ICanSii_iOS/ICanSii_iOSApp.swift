15b06740e6cac705b1e21dd557b6477097d1a806
Dorianhgn <dorianhugonnet1@gmail.com>
2026-03-05 09:50:26 +0100

    Initial Commit

---
+++ ICanSii_iOS/ICanSii_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ICanSii_iOSApp.swift
+//  ICanSii_iOS
+//
+//  Created by Dorian Hugonnet on 05/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct ICanSii_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
